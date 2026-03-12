0c1dc3d73499cce192a9448e72c84e78ae8a5957
battybludfish <mvgb2005@outlook.com>
2026-03-12 07:14:06 +0000

    homepage created

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signup
+from .views import signup, home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('signup/', signup, name='signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -13,3 +13,6 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
