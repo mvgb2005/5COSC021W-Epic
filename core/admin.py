81e8e8cf69f2156388dc83659fd755b529071985
[Syed Abbas} <277956387+SyedDev92450@users.noreply.github.com>
2026-04-27 21:34:55 +0100

    Add import for Q object in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Department, Team, Membership, Repository, Dependency, ContactChannel, AuditLog
+from django.db.models import Q
 
 # Register your models here.
 admin.site.register(Department)
