c534d466158eff78f526c6aee8d2fdde6372b329
[Syed Abbas} <277956387+SyedDev92450@users.noreply.github.com>
2026-04-28 04:52:14 +0100

    Add ScheduleMeetingAdmin to enhance admin interface for schedule meetings

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
 from .models import ScheduleMeeting
 
+
+# show schedule meetings in admin
+class ScheduleMeetingAdmin(admin.ModelAdmin):
+    list_display = (
+        'meetingTitle',
+        'meetingDate',
+        'meetingTime',
+        'meetingPlatform',
+        'scheduleType',
+        'createdBy',
+    )
+
+    # admin filters
+    list_filter = (
+        'meetingDate',
+        'createdBy',
+        'meetingPlatform',
+        'scheduleType',
+    )
+
+    # admin search
+    search_fields = (
+        'meetingTitle',
+        'meetingMessage',
+        'createdBy__username',
+    )
+
+
 # register schedule model in admin panel
-admin.site.register(ScheduleMeeting)
+admin.site.register(ScheduleMeeting, ScheduleMeetingAdmin)
