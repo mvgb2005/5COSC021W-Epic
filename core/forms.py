6c3471bd42ff0d1300e7e4b5cde9c50f7ccefd01
mohul <mvgb2005@outlook.com>
2026-04-30 01:31:08 +0100

    Add UserForm with first and last name fields
    Aurouba

--- core/forms.py
+++ core/forms.py
@@ -11,3 +11,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class UserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', )
